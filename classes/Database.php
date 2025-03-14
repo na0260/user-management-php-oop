febe99edd2d3866f0b0adfc1188d1df85387ff39
na0260 <samjr.008@gmail.com>
2025-03-15 02:16:36 +0600

    init with db con

---
+++ classes/Database.php
@@ -0,0 +1,24 @@
+<?php
+namespace Classes;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private $host = 'localhost';
+    private $db_name = 'user_management';
+    private $username = 'root';
+    private $password = '';
+    public $conn;
+
+    public function __construct()
+    {
+        try {
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+}
