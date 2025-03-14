3c6df66a29a345464bcebba3d4d1c54010a879cf
na0260 <samjr.008@gmail.com>
2025-03-15 02:34:15 +0600

    login and logout file added

---
+++ public/login.php
@@ -0,0 +1,21 @@
+<?php
+use Classes\User;
+
+$user = new User();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if ($user->login($email, $password)) {
+        header("Location: index.php");
+    } else {
+        echo "Login failed!";
+    }
+}
+?>
+<form method="post">
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Login</button>
+</form>
